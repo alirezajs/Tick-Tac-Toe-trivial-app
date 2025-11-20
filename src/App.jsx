ae53e834103370b3a8349e21fedee1171d1cf3b0
varmaghani <alireza.varmaghani@gmail.com>
2025-11-20 20:14:17 +0100

    refactor: simplify active player logic and remove redundant state management

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,22 @@ import Player from './components/player';
 import GameBoard from './components/GameBoard';
 import Log from './components/Log';
 
+function deriveActivePlayer(gameTurns) {
+  let currentPlayer = 'X';
+  if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
+    currentPlayer = 'O';
+  }
+  return currentPlayer;
+}
+
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
-  const [activePlayer, setActivePlayer] = useState('X');
+
+  const currentPlayer = deriveActivePlayer(gameTurns);
 
   function handleSelectSquare(rowIndex, colIndex) {
-    setActivePlayer((currentPlayer) => (currentPlayer === 'X' ? 'O' : 'X'));
-
     setGameTurns((prevTurn) => {
-      let currentPlayer = 'X';
-      if (prevTurn.length > 0 && prevTurn[0].player === 'X') {
-        currentPlayer = 'O';
-      }
+      const currentPlayer = deriveActivePlayer(prevTurn);
       const updatedTurn = [
         { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
         ...prevTurn,
@@ -27,8 +31,8 @@ function App() {
     <>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          <Player name="PLAYER 1" symbol="X" isActive={activePlayer === 'X'} />
-          <Player name="PLAYER 2" symbol="O" isActive={activePlayer === 'O'} />
+          <Player name="PLAYER 1" symbol="X" isActive={currentPlayer === 'X'} />
+          <Player name="PLAYER 2" symbol="O" isActive={currentPlayer === 'O'} />
         </ol>
         <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
       </div>
